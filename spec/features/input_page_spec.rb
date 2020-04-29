d2dea6de6a5d7034af4f407b0a705a87e72a6ab3
dpwdec <declankolakowski@gmail.com>
2020-04-29 11:57:13 +0100

    Added html button with submit

--- spec/features/input_page_spec.rb
+++ spec/features/input_page_spec.rb
@@ -5,6 +5,17 @@ feature 'Home page greeting' do
   end
 
   scenario 'It has a form with a name field' do
-    expect(page).to have_field('Name')
+    visit('/')
+    expect(page).to have_field('name')
+  end
+
+  scenario 'The form name field is named' do
+    visit('/')
+    expect(page).to have_content("What's your name?")
+  end
+
+  scenario 'It has a submit button' do
+    visit('/')
+    expect(page).to have_button('Submit')
   end
 end
