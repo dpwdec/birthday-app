00acffdaaf5e0271e0f84bdd4dba846dfd6e9f07
dpwdec <declankolakowski@gmail.com>
2020-04-30 12:10:58 +0100

    created user object to store birthday information:

---
+++ lib/user.rb
@@ -0,0 +1,9 @@
+class User
+  attr_reader :name, :day, :month
+
+  def initialize(name, day, month)
+    @name = name
+    @day = day
+    @month = month
+  end
+end
--- spec/user_spec.rb
+++ spec/user_spec.rb
@@ -1,1 +1,18 @@
 require 'user'
+
+describe User do
+
+  let(:subject) { described_class.new('Dec', '17', 'September') }
+
+  it "returns the user's name" do
+    expect(subject.name).to eq('Dec')
+  end
+
+  it "returns the user's birthday day" do
+    expect(subject.name).to eq('Dec')
+  end
+
+  it "returns the user's month" do
+    expect(subject.name).to eq('Dec')
+  end
+end
