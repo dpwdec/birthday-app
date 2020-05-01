f6845092bf459bbc6cec7607def33f08c77e0c2d
dpwdec <declankolakowski@gmail.com>
2020-05-01 10:56:45 +0100

    refactored feature tests to singular interface method.

--- spec/features/input_page_spec.rb
+++ spec/features/input_page_spec.rb
@@ -1,22 +1,14 @@
 feature 'Home page greeting' do
-  scenario 'It says "Hello there!"' do
+
+  scenario 'It has interface for the index' do
     visit('/')
     expect(page).to have_content('Hello there!')
-  end
-
-  scenario 'It has a form with a name field' do
-    visit('/')
     expect(page).to have_field('name')
-  end
-
-  scenario 'The form name field is named' do
-    visit('/')
     expect(page).to have_content("What's your name?")
-  end
-
-  scenario 'It has a submit button' do
-    visit('/')
     expect(page).to have_button('Submit')
+    expect(page).to have_content("When's your birthday?")
+    expect(page).to have_field('day')
+    expect(page).to have_field('month')
   end
 
   scenario 'Clicking on the submit button takes you to the greet page' do
@@ -26,28 +18,6 @@ feature 'Home page greeting' do
     find_by_id('month').find(:xpath, "option[#{(Date.today.month)}]").select_option
     click_button('Submit')
     expect(page).to have_current_path('/greet')
-  end
-
-  # scenario 'Greet page contains the name entered' do
-  #   visit('/')
-  #   fill_in('name', with: 'John')
-  #   click_button('Submit')
-  #   expect(page).to have_content('John')
-  # end
-
-  scenario 'It has a label asking for your birthday' do
-    visit('/')
-    expect(page).to have_content("When's your birthday?")
-  end
-
-  scenario 'It has a a field for day' do
-    visit('/')
-    expect(page).to have_field('day')
-  end
-
-  scenario 'It has a dropdown for month' do
-    visit('/')
-    expect(page).to have_field('month')
   end
 
   scenario 'Greet page says "Happy Birthday" when your birthday is today' do
